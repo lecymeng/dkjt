63bb08658719599c68406c1469b72bf7c6551903
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-01-09 09:45:29 +0800

    use TabLayout, add design library

--- app/src/main/java/com/sspai/dkjt/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/sspai/dkjt/ui/fragment/HomeFragment.java
@@ -1,22 +1,19 @@
 package com.sspai.dkjt.ui.fragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import com.astuetz.PagerSlidingTabStrip;
-import com.sspai.dkjt.model.DeviceProvider;
 import com.sspai.dkjt.R;
-import com.sspai.dkjt.model.Utils;
+import com.sspai.dkjt.model.BooleanPreference;
+import com.sspai.dkjt.model.DeviceProvider;
 import com.sspai.dkjt.prefs.GlareEnabled;
 import com.sspai.dkjt.prefs.ShadowEnabled;
-import com.sspai.dkjt.model.BooleanPreference;
 import com.sspai.dkjt.ui.adapter.DeviceFragmentPagerAdapter;
-
 import javax.inject.Inject;
 
 /**
@@ -31,7 +28,8 @@ public class HomeFragment extends BaseFragment {
   @Inject DeviceProvider deviceProvider;
   @Inject WindowManager windowManager;
   private ViewPager pager;
-  private PagerSlidingTabStrip tabStrip;
+  //private PagerSlidingTabStrip tabStrip;
+  private TabLayout mTabLayout;
   DeviceFragmentPagerAdapter pagerAdapter;
   private String mCategory;
 
@@ -48,7 +46,8 @@ public class HomeFragment extends BaseFragment {
   public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_home, container, false);
     pager = rootView.findViewById(R.id.pager);
-    tabStrip = rootView.findViewById(R.id.tabs);
+    //tabStrip = rootView.findViewById(R.id.tabs);
+    mTabLayout=rootView.findViewById(R.id.tab_layout);
     parseArgument();
     initView();
     return rootView;
@@ -58,8 +57,9 @@ public class HomeFragment extends BaseFragment {
     pagerAdapter = new DeviceFragmentPagerAdapter(getFragmentManager(), deviceProvider.asList(mCategory));
     pager.setAdapter(pagerAdapter);
     pager.setCurrentItem(pagerAdapter.getDeviceIndex(deviceProvider.getDefaultDevice()));
-    tabStrip.setTextColor(Utils.getColor(getActivity(), R.color.title_text_color, Color.WHITE));
-    tabStrip.setViewPager(pager);
+    //tabStrip.setTextColor(Utils.getColor(getActivity(), R.color.title_text_color, Color.WHITE));
+    //tabStrip.setViewPager(pager);
+    mTabLayout.setupWithViewPager(pager);
   }
 
   //获得分类字段
